3607fc6351792af302183fee8f04d6a833e51047
GlamorousBrusier12 <pandurebelsachin18@gmail.com>
2024-06-02 17:55:24 +0530

    add dummy authenticated user to context

--- graphqlapi/server.js
+++ graphqlapi/server.js
@@ -8,6 +8,19 @@ const server = new ApolloServer({
   resolvers,
 });
 
-const { url } = await startStandaloneServer(server);
+const { url } = await startStandaloneServer(server, {
+  context: ({ req, res }) => {
+    const token = req.headers.authorization || "";
+
+    // get the user from the token, jwt validation here
+    const user = {
+      id: 1,
+      name: "naveen",
+      email: "naveen.kalagarla18@gmail.com",
+    };
+
+    return { jwtUser: user };
+  },
+});
 
 console.log(`🚀 Server ready at ${url}`);
